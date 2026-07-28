60508c3624eaccd4a5ad7084783d094159292900
Rodrigo de Souza Sabino <implantarh.consultoria@gmail.com>
2026-07-28 14:05:00 -0300

    fix: carregar bloqueio visual da toolbar Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/topac-platform.css";
+import "./styles/hide-vercel-toolbar.css";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 const MOBILE_BUILD_TAG = "20260605-acesso-final-1";
