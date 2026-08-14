554f2c5ed9f31f1cc331e345a342b8550b5ece1e
Rodrigo de Souza Sabino <implantarh.consultoria@gmail.com>
2026-08-14 14:07:34 -0300

    security: fechar acesso operacional da diretoria ao EPI

--- src/lib/directorPermissions.ts
+++ src/lib/directorPermissions.ts
@@ -2,7 +2,6 @@ export const DIRECTOR_BLOCKED_MESSAGE = 'Edicao bloqueada para este perfil. Soli
 
 const DIRECTOR_ALLOWED_ADMIN_PREFIXES = [
   '/admin/diretoria',
-  '/admin/epi',
   '/admin/faturamento',
   '/admin/faturamento/migracao-dn4',
   '/admin/financeiro',
@@ -19,6 +18,7 @@ const DIRECTOR_BLOCKED_EDIT_PREFIXES = [
   '/admin/fechamento',
   '/admin/fechamento-ponto',
   '/admin/fechamentos-filiais',
+  '/admin/epi',
   '/admin/uniformes',
   '/admin/aso',
   '/admin/prestadores',
