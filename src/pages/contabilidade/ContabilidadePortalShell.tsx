267ffd94a25eb20052455331aaa6c567f2a13c5a
Rodrigo de Souza Sabino <implantarh.consultoria@gmail.com>
2026-09-15 14:58:47 -0300

    feat: add correction option to accounting portal

--- src/pages/contabilidade/ContabilidadePortalShell.tsx
+++ src/pages/contabilidade/ContabilidadePortalShell.tsx
@@ -1,6 +1,7 @@
 import ContabilidadeDashboardPage from './ContabilidadeDashboardPage';
 import ContabilidadePortalEmailBridge from './ContabilidadePortalEmailBridge';
 import ContabilidadeFolhaFluxo from './ContabilidadeFolhaFluxo';
+import ContabilidadeCorrectionPanel from '@/components/ContabilidadeCorrectionPanel';
 
 type PortalKind = 'principal' | 'goiania';
 
@@ -10,6 +11,7 @@ export default function ContabilidadePortalShell({ portal }: { portal: PortalKin
       <ContabilidadePortalEmailBridge />
       <div className="px-4 pt-4 sm:px-6 lg:px-8">
         <ContabilidadeFolhaFluxo portal={portal} />
+        <ContabilidadeCorrectionPanel mode="portal" portal={portal} compact />
       </div>
       <ContabilidadeDashboardPage portal={portal} />
     </>
